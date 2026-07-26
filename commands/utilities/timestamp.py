d0994c9dfbb920ab6f07446830b902311c635bae
Zane <zkutchinsky4413@gmail.com>
2026-07-25 20:36:47 -0700

    Update timestamp.py

--- commands/utilities/timestamp.py
+++ commands/utilities/timestamp.py
@@ -1,1 +1,25 @@
+"""Usage: !timestamp (current time) or !timestamp 1735689600 (a unix timestamp)"""
 
+import time
+
+
+async def run(client, message, args):
+    """Converts a unix timestamp (or now) into Discord's clickable time format"""
+    args = args.strip()
+
+    if args:
+        try:
+            unix = int(args)
+        except ValueError:
+            await message.channel.send("That's not a valid unix timestamp (whole number of seconds).")
+            return
+    else:
+        unix = int(time.time())
+
+    lines = [
+        f"Short: <t:{unix}:t>",
+        f"Long: <t:{unix}:F>",
+        f"Relative: <t:{unix}:R>",
+        f"Raw: `{unix}`",
+    ]
+    await message.channel.send("\n".join(lines))
